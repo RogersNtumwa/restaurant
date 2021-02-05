6f54fa2d5491ef13006a21d986bdb2756c528816
Rogers Ntumwa <ntumwar@gmail.com>
2021-02-05 19:59:39 +0400

    initialised redux into the application

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
---
+++ src/store.js
@@ -0,0 +1,48 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+// import {
+//   productsList,
+//   productDetails,
+//   deleteProductReducer,
+//   createProductReducer,
+//   editProductReducer,
+//   relatedProductsReducer,
+// } from "./reducers/products";
+// import { cartReducer } from "./reducers/cart";
+// import user from "./reducers/auth";
+// import {
+//   PlaceOrderReducer,
+//   OrderDetailsReducer,
+//   OrderReducer,
+//   deleteOrderReducer,
+// } from "./reducers/order";
+// import { Users } from "./reducers/users";
+import alertReducer from "./reducers/alert";
+
+// const cartItemsFromStorage = localStorage.getItem("cartItems")
+//   ? JSON.parse(localStorage.getItem("cartItems"))
+//   : [];
+// const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+//   ? JSON.parse(localStorage.getItem("shippingAddress"))
+//   : {};
+
+// const inititalState = {
+//   cart: {
+//     cartItems: cartItemsFromStorage,
+//     shippingAddress: shippingAddressFromStorage,
+//   },
+// };
+const middleware = [thunk];
+
+const reducer = combineReducers({
+  alert: alertReducer,
+});
+
+const store = createStore(
+  reducer,
+  inititalState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
